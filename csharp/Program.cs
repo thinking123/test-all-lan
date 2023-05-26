0d0dc32a25bb851449b5acf93f25fb607bfc83c0
thinking123 <1294873004@qq.com>
2023-05-26 15:59:17 +0800

    add c

---
+++ csharp/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+
+
+public class HelloWorld
+{
+
+
+
+  public static void Main()
+  {
+ 
+    Console.WriteLine("res1");
+    //1
+  }
+}
+
